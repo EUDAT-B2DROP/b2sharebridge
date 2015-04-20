081b4aa36e121ff1137ef0097ad9e39b1b90cdf4
BeneDicere <b.von.st.vieth@fz-juelich.de>
2015-04-20 10:40:12 +0200

    this should provide the admin a means to configure the b2share api endpoint.

---
+++ settings.php
@@ -0,0 +1,22 @@
+<?php
+
+OC_Util::checkAdminUser();
+
+OCP\Util::addScript('eudat_b2share', 'settings');
+
+$params = array('b2share_endpoint_url', 'b2share_bridge_enabled');
+  
+if($_POST) {
+	foreach($params as $param) {
+		if (isset($_POST[$param])) {
+			OCP\Config::setAppValue('eudat_b2share', $param, $_POST[$param]);
+		}
+	}
+}
+
+// fill template
+$tmpl = new OCP\Template( 'eudat_b2share', 'settings-admin');
+$tmpl->assign('b2share_endpoint_url', OCP\Config::getAppValue('eudat_b2share', 'b2share_endpoint_url', ''));
+$tmpl->assign('b2share_bridge_enabled', OCP\Config::getAppValue('eudat_b2share', 'b2share_bridge_enabled', '0'));
+
+return $tmpl->fetchPage();
---
+++ templates/settings.php
@@ -0,0 +1,23 @@
+<?php
+/** @var OC_L10N $l */
+/** @var array $_ */
+?>
+<div id="eudat_b2share" action="#" method="post">
+    <h3><?php p($l->t('EUDAT B2SHARE Bridge'));?></h3>
+
+    <p>
+        <input type="text" name="b2share_endpoint_url" id="b2share_endpoint_url"
+               value="<?php p($_['b2share_endpoint_url']); ?>" />
+        <label for="b2share_endpoint_url">
+            <?php p($l->t('External B2SHARE API endpoint'));?>
+        </label>
+    </p>
+
+    <p>
+        <input type="checkbox" name="b2share_bridge_enabled" id="b2share_bridge_enabled"
+               value="1" <?php if ($_['incomingServer2serverShareEnabled']) print_unescaped('checked="checked"'); ?> />
+        <label for="b2share_bridge_enabled">
+            <?php p($l->t('Publishing to B2SHARE enabled'));?>
+        </label><br/>
+    </p>
+</div>

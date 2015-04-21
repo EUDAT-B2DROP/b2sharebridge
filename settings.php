d14df87011a2e045d63a336228fdc87344a955f1
BeneDicere <b.von.st.vieth@fz-juelich.de>
2015-04-21 11:20:58 +0200

    next try for config manipulation

--- settings.php
+++ settings.php
@@ -7,15 +7,13 @@ $params = array('b2share_endpoint_url', 'b2share_bridge_enabled');
 if($_POST) {
 	foreach($params as $param) {
 		if (isset($_POST[$param])) {
-			OCP\IConfig::setAppValue('eudat', $param, $_POST[$param]);
+			OCP\Config::setAppValue('eudat', $param, $_POST[$param]);
 		}
 	}
 }
-#OCP\IConfig::setAppValue('eudat', 'b2share_endpoint_url', 'foo');
-#OCP\IConfig::setAppValue('eudat', 'b2share_endpoint_url', 'foo');
 // fill template
 $tmpl = new OCP\Template( 'eudat', 'settings');
-$tmpl->assign('b2share_endpoint_url', OCP\IConfig::getAppValue('eudat', 'b2share_endpoint_url', 'asd'));
-$tmpl->assign('b2share_bridge_enabled', OCP\IConfig::getAppValue('eudat', 'b2share_bridge_enabled', '0'));
+$tmpl->assign('b2share_endpoint_url', OCP\Config::getAppValue('eudat', 'b2share_endpoint_url', 'asd'));
+$tmpl->assign('b2share_bridge_enabled', OCP\Config::getAppValue('eudat', 'b2share_bridge_enabled', '0'));
 
 return $tmpl->fetchPage();

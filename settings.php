50b0b56dd9680a92687f8746d59f91dee807a013
BeneDicere <b.von.st.vieth@fz-juelich.de>
2015-04-22 12:17:37 +0200

    test config management

--- settings.php
+++ settings.php
@@ -2,20 +2,20 @@
 
 OC_Util::checkAdminUser();
 
-$appConfig = \OC::$server->getAppConfig();
+$config = \OC::$server->getConfig();
 
 $params = array('b2share_endpoint_url', 'b2share_bridge_enabled');
 
 if($_POST) {
 	foreach($params as $param) {
 		if (isset($_POST[$param])) {
-            $appConfig->setAppValue('eudat', $param, $_POST[$param]);
+            $config->setAppValue('eudat', $param, $_POST[$param]);
 		}
 	}
 }
 // fill template
 $tmpl = new OCP\Template( 'eudat', 'settings');
-$tmpl->assign('b2share_bridge_enabled', $appConfig->getAppValue('eudat', 'b2share_bridge_enabled', '1'));
-$tmpl->assign('b2share_endpoint_url', $appConfig->getAppValue('eudat', 'b2share_endpoint_url', 'asd'));
+$tmpl->assign('b2share_bridge_enabled', $config->getAppValue('eudat', 'b2share_bridge_enabled', '1'));
+$tmpl->assign('b2share_endpoint_url', $config->getAppValue('eudat', 'b2share_endpoint_url', 'asd'));
 
 return $tmpl->fetchPage();

fce47d69027088ecece0cf82a2842016ef40493b
BeneDicere <b.von.st.vieth@fz-juelich.de>
2015-04-22 12:18:30 +0200

    test config management

--- settings.php
+++ settings.php
@@ -15,7 +15,7 @@ if($_POST) {
 }
 // fill template
 $tmpl = new OCP\Template( 'eudat', 'settings');
-$tmpl->assign('b2share_bridge_enabled', $config->getAppValue('eudat', 'b2share_bridge_enabled', '1'));
+$tmpl->assign('b2share_bridge_enabled', $config->getAppValue('eudat', 'b2share_bridge_enabled', 'no'));
 $tmpl->assign('b2share_endpoint_url', $config->getAppValue('eudat', 'b2share_endpoint_url', 'asd'));
 
 return $tmpl->fetchPage();

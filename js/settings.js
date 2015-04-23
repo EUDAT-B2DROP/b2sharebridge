9e20267199a03f483b4f6942d738bb0f53b41257
BeneDicere <b.von.st.vieth@fz-juelich.de>
2015-04-23 14:41:01 +0200

    improve php and js

--- js/settings.js
+++ js/settings.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
         var value = 'no';
         if (this.checked) {
             value = 'yes';
-            $('#b2share_url').removeClass('hidden');
+            $('#b2share_url_field').removeClass('hidden');
         } else {
-            $('#b2share_url').addClass('hidden');
+            $('#b2share_url_field').addClass('hidden');
         }
         OC.AppConfig.setValue('eudat', $(this).attr('name'), value);
     });
--- templates/settings.php
+++ templates/settings.php
@@ -9,7 +9,7 @@
                value="1" <?php if ($_['b2share_bridge_enabled'] === 'yes') print_unescaped('checked="checked"'); ?> />
         <label for="b2share_enabled"><?php p($l->t('Publishing to B2SHARE enabled'));?></label><br/>
     </p>
-    <p <?php if ($_['b2share_bridge_enabled'] === 'no') print_unescaped('class="hidden"'); ?>>
+    <p id="b2share_url_field" <?php if ($_['b2share_bridge_enabled'] === 'no') print_unescaped('class="hidden"'); ?>>
         <input type="text" name="b2share_url" id="b2share_url" placeholder="https://b2share.eudat.eu"
                value="<?php p($_['b2share_endpoint_url']); ?>" />
         <label for="b2share_endpoint_url"><?php p($l->t('External B2SHARE API endpoint'));?></label><br/>

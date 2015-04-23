ad976957478f50a27103bdc75bd03397ef484e81
BeneDicere <b.von.st.vieth@fz-juelich.de>
2015-04-23 13:06:50 +0200

    remove unused php stuff.

--- settings.php
+++ settings.php
@@ -4,16 +4,6 @@ OC_Util::checkAdminUser();
 
 $config = \OC::$server->getConfig();
 
-$params = array('b2share_endpoint_url', 'b2share_bridge_enabled');
-
-if($_POST) {
-	foreach($params as $param) {
-		if (isset($_POST[$param])) {
-            $config->setAppValue('eudat', $param, $_POST[$param]);
-		}
-	}
-}
-// fill template
 $tmpl = new OCP\Template( 'eudat', 'settings');
 $tmpl->assign('b2share_bridge_enabled', $config->getAppValue('eudat', 'b2share_bridge_enabled', 'no'));
 $tmpl->assign('b2share_endpoint_url', $config->getAppValue('eudat', 'b2share_endpoint_url', 'asd'));

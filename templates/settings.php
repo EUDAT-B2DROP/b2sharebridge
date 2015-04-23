b2626a59a37da39d5399bc3b23fcd402a4db9702
BeneDicere <b.von.st.vieth@fz-juelich.de>
2015-04-23 13:18:59 +0200

    improve settings tab for b2share.

--- templates/settings.php
+++ templates/settings.php
@@ -4,13 +4,13 @@
 ?>
 <div id="eudat_b2share" class="section">
     <h2><?php p($l->t('EUDAT B2SHARE Bridge'));?></h2>
-    <p id="b2share_enable">
+    <p>
         <input type="checkbox" name="b2share_enabled" id="b2share_enabled"
                value="1" <?php if ($_['b2share_bridge_enabled'] === 'yes') print_unescaped('checked="checked"'); ?> />
-        <label for="b2share_bridge_enabled"><?php p($l->t('Publishing to B2SHARE enabled'));?></label><br/>
+        <label for="b2share_enabled"><?php p($l->t('Publishing to B2SHARE enabled'));?></label><br/>
     </p>
-    <p id="b2share_endpoint" <?php if ($_['b2share_bridge_enabled'] === 'no') print_unescaped(' class="hidden"'); ?>
-        <input type="text" name="b2share_endpoint_url" id="b2share_endpoint_url"
+    <p>
+        <input type="text" name="b2share_url" id="b2share_url <?php if ($_['b2share_bridge_enabled'] === 'no') print_unescaped(' class="hidden"'); ?>"
                value="<?php p($_['b2share_endpoint_url']); ?>" />
         <label for="b2share_endpoint_url"><?php p($l->t('External B2SHARE API endpoint'));?></label><br/>
     </p>

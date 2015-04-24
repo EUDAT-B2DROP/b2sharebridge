e5d784227730398aaf6769c1c0cbcd75f52f38e4
BeneDicere <b.von.st.vieth@fz-juelich.de>
2015-04-24 08:26:08 +0200

    try to address elements via name instead of id.

--- templates/settings.php
+++ templates/settings.php
@@ -7,8 +7,8 @@
     <p name="b2share_url_field" id="b2shareUrlField">
         <input type="text" name="b2share_endpoint_url" id="b2shareUrl" placeholder="https://b2share.eudat.eu" style="width: 400px"
                value="<?php p($_['b2share_endpoint_url']); ?>" />
-        <span class="regexstatus success" id="regexstatusSuccess" style="visibility: hidden"></span>
-        <span class="regexstatus error" id="regexstatusError" style="visibility: hidden"></span>
+        <span class="regexstatus success" name="regexstatus_success" id="regexstatusSuccess" style="visibility: hidden"></span>
+        <span class="regexstatus error" name="regexstatus_error" id="regexstatusError" style="visibility: hidden"></span>
         <br />
         <em><?php p($l->t('External B2SHARE API endpoint')); ?></em>
     </p>

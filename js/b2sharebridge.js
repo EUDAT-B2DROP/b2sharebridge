e664ec0404da752e0cbebe7e03f0fe97c29d9310
BeneDicere <b.von.st.vieth@fz-juelich.de>
2015-05-06 15:29:15 +0200

    css working now. gui done, now functionality..

--- js/b2sharebridge.js
+++ js/b2sharebridge.js
@@ -1,65 +1,44 @@
 $(document).ready(function(){
-    if ($('#isPublic').val()){
-        // no versions actions in public mode
-        // beware of https://github.com/owncloud/core/issues/4545
-        // as enabling this might hang Chrome
-        return;
-    }
-
     if (OCA.Files) {
-        // Add versions button to 'files/index.php'
+        // Add b2share button to 'files/index.php'
         OCA.Files.fileActions.register('file', t('eudat', 'B2SHARE'), OC.PERMISSION_READ,
             function() {
             },
             function(filename, context){
-                // Action to perform when clicked
-                if (scanFiles.scanning){return;}//workaround to prevent additional http request block scanning feedback
-
-                var file = context.dir.replace(/(?!<=\/)$|\/$/, '/' + filename);
                 var createDropDown = true;
-                // Check if drop down is already visible for a different file
-                if (($('#b2dropdropdown').length > 0) ) {
-                    if ( $('#b2dropdropdown').hasClass('drop-b2drop') && file == $('#b2dropdropdown').data('file')) {
+                // Check if drop down is already visible
+                if ($('#dropdown').length > 0) {
+                    //is a b2share dropdown visible for the current file?
+                    if ( $('#dropdown').hasClass('drop-b2drop') && filename == $('#dropdown').data('file')) {
                         createDropDown = false;
                     }
-                    $('#b2dropdropdown').slideUp(OC.menuSpeed);
-                    $('#b2dropdropdown').remove();
-                    $('tr').removeClass('mouseOver');
+                    $('#dropdown').slideUp(OC.menuSpeed);
+                    $('#dropdown').remove();
                 }
-
                 if(createDropDown === true) {
-                    showDropDown(filename, file, context.fileList);
+                    showDropDown(filename, context.fileList);
                 }
             }
         );
+        $(this).click(function(event) {
+            if ( $(event.target).is('.drop-b2drop') && $('#dropdown').has(event.target).length === 0) {
+                $('#dropdown').slideUp(OC.menuSpeed);
+                $('#dropdown').remove();
+            }
+        });
     }
 });
 
-function showDropDown(filename, files, fileList) {
-    var html = '<div id="b2dropdropdown" class="drop-b2drop" data-item="'+escapeHTML(files)+'">';
-    //html += '<form action="transfer.php">';
+function showDropDown(filename, fileList) {
+    var html = '<div id="dropdown" class="drop-b2drop" data-item="' + escapeHTML(filename) + '">';
     html += '<a href="https://b2share.eudat.eu">token:</a>';
-    html += '<input id="b2share_token" type="text" value="" autofocus />';
-    html += '<input id="b2share_submit" type="submit" value="publish" />';
-    //html += '</form>';
+    html += '<input id="b2share_token" class="b2shareToken" type="text" value="" autofocus />';
+    html += '<input id="b2share_submit" class="b2shareSubmit" type="submit" value="publish" />';
 
     if (filename) {
         fileEl = fileList.findFileEl(filename);
         fileEl.addClass('mouseOver');
         $(html).appendTo(fileEl.find('td.filename'));
-    } else {
-        $(html).appendTo($('thead .share'));
     }
-    $('#b2dropdropdown').slideDown(1000);
+    $('#dropdown').slideDown(OC.menuSpeed);
 };
-
-$(this).click(
-    function(event) {
-        if ($('#b2dropdropdown').has(event.target).length === 0 && $('#b2dropdropdown').hasClass('drop-b2drop')) {
-            $('#b2dropdropdown').slideUp(OC.menuSpeed, function() {
-                $('#b2dropdropdown').remove();
-                $('tr').removeClass('mouseOver');
-            });
-        }
-    }
-);

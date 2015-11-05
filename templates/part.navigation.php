b132c3c7fef564e037acca417f10123e3768eeb5
Benedikt von St. Vieth <b.von.st.vieth@fz-juelich.de>
2015-11-05 09:21:43 +0100

    remove unused item from navigation bar

--- templates/part.navigation.php
+++ templates/part.navigation.php
@@ -1,10 +1,12 @@
 <ul>
-    <li><a href="">Status</a></li>
     <li>
+        <a href="">Status</a>
+    </li>
+    <!--<li>
         <a href="#">First level container</a>
         <ul>
             <li><a href="#">Second level entry</a></li>
             <li><a href="#">Second level entry</a></li>
         </ul>
-    </li>
+    </li>-->
 </ul>

55c67454a6d73ccb3ddc4698d1c2b2423888a77f
Sander Apweiler <sa.apweiler@fz-juelich.de>
2016-09-12 15:08:16 +0200

    expand unit test to new b2sharebridge controller

--- tests/unit/controller/ControllerTest.php
+++ tests/unit/controller/ControllerTest.php
@@ -26,16 +26,19 @@ class PageControllerTest extends PHPUnit_Framework_TestCase {
         $mapper = $this->getMockBuilder('OCA\B2shareBridge\Db\FilecacheStatusMapper')
             ->disableOriginalConstructor()
             ->getMock();
-
+        $scMapper = $this->getMockBuilder('OCA\B2shareBridge\Db\StatusCodeMapper')
+            ->disableOriginalConstructor()
+            ->getMock();
+            
 
         $this->controller = new B2shareBridge(
-            'b2sharebridge', $request, $config, $mapper, $this->userId
+            'b2sharebridge', $request, $config, $mapper, $scMapper, $this->userId
         );
     }
 
     public function testIndex() {
         $result = $this->controller->index();
-        $this->assertEquals(['user' => 'john', 'transfers' => Array (), 'publications' => Array (), 'fails' => Array ()], $result->getParams());
+        $this->assertEquals(['user' => 'john', 'transfers' => Array (), 'publications' => Array (), 'fails' => Array (), 'statuscodes' => Array ()], $result->getParams());
         $this->assertEquals('main', $result->getTemplateName());
         $this->assertTrue($result instanceof TemplateResponse);
     }

2f533aae40e1714fad8cd5eb719b5cf33c12d917
Sander Apweiler <sa.apweiler@fz-juelich.de>
2016-09-12 11:51:30 +0200

    add simple status code mapper

---
+++ lib/db/statuscodemapper.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * OwnCloud - B2sharebridge App
+ *
+ * PHP Version 5-7
+ *
+ * @category  Owncloud
+ * @package   B2shareBridge
+ * @author    EUDAT <b2drop-devel@postit.csc.fi>
+ * @copyright 2015 EUDAT
+ * @license   AGPL3 https://github.com/EUDAT-B2DROP/b2sharebridge/blob/master/LICENSE
+ * @link      https://github.com/EUDAT-B2DROP/b2sharebridge.git
+ */
+
+namespace OCA\B2shareBridge\Db;
+
+use OCP\AppFramework\Db\Entity;
+use OCP\IDBConnection;
+use OCP\AppFramework\Db\Mapper;
+
+/**
+ * Work on a database table
+ *
+ * @category Owncloud
+ * @package  B2shareBridge
+ * @author   EUDAT <b2drop-devel@postit.csc.fi>
+ * @license  AGPL3 https://github.com/EUDAT-B2DROP/b2sharebridge/blob/master/LICENSE
+ * @link     https://github.com/EUDAT-B2DROP/b2sharebridge.git
+ */
+class StatusCodeMapper extends Mapper
+{
+
+    /**
+     * Create the database mapper
+     *
+     * @param IDBConnection $db the database connection to use
+     */
+    public function __construct(IDBConnection $db)
+    {
+        parent::__construct(
+            $db,
+            'b2sharebridge_status_code',
+            '\OCA\B2shareBridge\Db\StatusCode'
+        );
+    }
+
+    /**
+     * Find a database entry for a status code
+     *
+     * @param string $status_code statusCode to find a transfer entry for
+     *
+     * @throws \OCP\AppFramework\Db\DoesNotExistException if not found
+     * @throws \OCP\AppFramework\Db\MultipleObjectsReturnedException if more th one
+     *
+     * @return Entity
+     */
+    public function find($status_code)
+    {
+        $sql = 'SELECT * FROM `*PREFIX*b2sharebridge_status_code` '
+            . 'WHERE `status_code` = ?';
+        return $this->findEntity($sql, [$status_code]);
+    }
+
+}

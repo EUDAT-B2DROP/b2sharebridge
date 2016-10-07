5fc8b4be30eeeb0598891c6a518e99bafb994f21
Benedikt von St. Vieth <benedikt@zam476.zam.kfa-juelich.de>
2016-10-07 09:41:00 +0200

    slim down style.css, update navigation.php to use id app-navigation instead of app-b2sharebridge.

--- templates/navigation.php
+++ templates/navigation.php
@@ -1,4 +1,4 @@
-<div id="app-b2sharebridge">
+<div id="app-navigation">
     <ul class="with-icon">
         <li>
             <a href="all">All Deposits</a>
@@ -16,14 +16,29 @@
 </div>
 
 <!--<div id="app-navigation">
-    <ul class="with-icon">
-        <?php /*foreach ($_['navigationItems'] as $item) { */?>
-            <li data-id="<?php /*p($item['id']) */?>" class="nav-<?php /*p($item['id']) */?>">
-                <a href="<?php /*p(isset($item['href']) ? $item['href'] : '#') */?>"
-                   class="nav-icon-<?php /*p($item['icon'] !== '' ? $item['icon'] : $item['id']) */?> svg">
-                    <?php /*p($item['name']);*/?>
+    <?php /*foreach ($_['navigations'] as $navigationGroup => $navigationEntries) { */?>
+        <?php /*if ($navigationGroup !== 'apps'): */?><ul><?php /*endif; */?>
+
+        <?php /*foreach ($navigationEntries as $navigation) { */?>
+            <li<?php /*if ($_['activeNavigation'] === $navigation['id']): */?> class="active"<?php /*endif; */?>>
+                <a data-navigation="<?php /*p($navigation['id']) */?>" href="<?php /*p($navigation['url']) */?>">
+                    <?php /*p($navigation['name']) */?>
                 </a>
             </li>
         <?php /*} */?>
-    </ul>
+
+        <?php /*if ($navigationGroup !== 'top'): */?></ul><?php /*endif; */?>
+    <?php /*} */?>
+
+    <div id="app-settings">
+        <div id="app-settings-header">
+            <button class="settings-button" data-apps-slide-toggle="#app-settings-content"></button>
+        </div>
+
+        <div id="app-settings-content">
+            <input type="checkbox"<?php /*if ($_['rssLink']): */?> checked="checked"<?php /*endif; */?> id="enable_rss" class="checkbox" />
+            <label for="enable_rss"><?php /*p($l->t('Enable RSS feed'));*/?></label>
+            <input id="rssurl"<?php /*if (!$_['rssLink']): */?> class="hidden"<?php /*endif; */?> type="text" readonly="readonly" value="<?php /*p($_['rssLink']); */?>" />
+        </div>
+    </div>
 </div>-->

5684beb8e223ad94603c0fc55c7d648bdb3ab01a
bne86 <b.von.st.vieth@fz-juelich.de>
2017-03-16 08:29:07 +0100

    slim down code that generates the routes, and add a unittest for routes too.

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * OwnCloud - B2sharebridge App
+ * B2SHAREBRIDGE
  *
  * Create your routes in here. The name is the lowercase name of the Controller
  * without the Controller part, the stuff after the hash is the method.
@@ -21,15 +21,31 @@
  * @link      https://github.com/EUDAT-B2DROP/b2sharebridge.git
  */
 
-$application = new OCA\B2shareBridge\AppInfo\Application();
 
-$application->registerRoutes(
-    $this,
-    ['routes' => [
-        ['name' => 'Publish#publish', 'url' => '/publish', 'verb' => 'POST'],
-        ['name' => 'View#depositList', 'url' => '/', 'verb' => 'GET'],
-        ['name' => 'View#set_token', 'url' => '/apitoken', 'verb' => 'POST'],
-        ['name' => 'View#delete_token', 'url' => '/apitoken', 'verb' => 'DELETE'],
+namespace OCA\B2sharebridge\AppInfo;
+
+return [
+    'routes' => [
+        [
+            'name' => 'Publish#publish',
+            'url' => '/publish',
+            'verb' => 'POST'
+        ],
+        [
+            'name' => 'View#depositList',
+            'url' => '/',
+            'verb' => 'GET'
+        ],
+        [
+            'name' => 'View#set_token',
+            'url' => '/apitoken',
+            'verb' => 'POST'
+        ],
+        [
+            'name' => 'View#delete_token',
+            'url' => '/apitoken',
+            'verb' => 'DELETE'
+        ],
         [
             'name' => 'View#getTabViewContent',
             'url'=>'/gettabviewcontent',
@@ -40,5 +56,5 @@ $application->registerRoutes(
             'url' => '/initializeb2shareui',
             'verb' => 'GET'
         ],
-    ]]
-);
+    ]
+];

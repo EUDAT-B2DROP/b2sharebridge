5c83d354ce7a67fb0ae2c80a7ffbe15dc90da6a8
Thijs Cobben <thijscobben@gmail.com>
2017-08-16 15:01:45 +0200

    Debugging unit tests

--- lib/Controller/ViewController.php
+++ lib/Controller/ViewController.php
@@ -108,6 +108,7 @@ class ViewController extends Controller
                 ) as $publication) {
                     $publications[] = $publication;
             }
+     
         } else {
             foreach (
                 array_reverse(

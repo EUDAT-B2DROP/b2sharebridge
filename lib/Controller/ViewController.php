c1bc73899fd36489c727556c9d368dec23686685
Sander Apweiler <sa.apweiler@fz-juelich.de>
2019-02-28 14:46:57 +0100

    Insert link to user settings, if B2SHARE API token is empty. It is part of #48

--- lib/Controller/ViewController.php
+++ lib/Controller/ViewController.php
@@ -252,7 +252,7 @@ class ViewController extends Controller
         $token = $this->config->getUserValue($userId, $this->appName, 'token');
         if (!(strlen($token)>1)) {
             $is_error = true;
-            $error_msg .= "Please set B2SHARE API token<br>\n";
+            $error_msg .= "Please set B2SHARE API token <a href=\"../../settings/user/additional\" style=\"text-decoration: underline; color:red;\">here</a><br>\n";
         }
         $allowed_uploads = $this->config->getAppValue(
             'b2sharebridge',

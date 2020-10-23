3b5fa916c50f1802e6a07efdd9dc639ede09343c
Janne Karjalainen <janne.karjalainen@csc.fi>
2020-10-23 15:08:59 +0300

    Add post migrate script

--- lib/Migration/Version010100Date20200324092922.php
+++ lib/Migration/Version010100Date20200324092922.php
@@ -8,11 +8,16 @@ use Closure;
 use OCP\DB\ISchemaWrapper;
 use OCP\Migration\SimpleMigrationStep;
 use OCP\Migration\IOutput;
-
+use OCP\DB\QueryBuilder\IQueryBuilder;
+use OCP\IDBConnection;
 /**
  * Auto-generated migration step: Please modify to your needs!
  */
 class Version010100Date20200324092922 extends SimpleMigrationStep {
+
+	public function __construct(IDBConnection $db) {
+		$this->db = $db;
+	}
 
 	/**
 	 * @param IOutput $output
@@ -48,6 +53,62 @@ class Version010100Date20200324092922 extends SimpleMigrationStep {
 	 * @param array $options
 	 */
 	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
-		// TODO: migrate possible existing single-server configuration
+		$logger = \OC::$server->getLogger();
+
+		// get old B2SHARE URL
+		$q = $this->db->getQueryBuilder();
+		$q->select('a.configvalue')
+			->from('appconfig', 'a')
+			->where('a.appid =' . $q->createParameter('appid'))
+			->andWhere('a.configkey =' . $q->createParameter('configkey'))
+			->setParameter('appid', 'b2sharebridge')
+			->setParameter('configkey', 'publish_baseurl');
+		$arr = $q->execute()->fetch();
+
+		if ($arr) {
+			// create row in new server table for old URL
+			$q = $this->db->getQueryBuilder();
+			$q->insert('b2sharebridge_server')
+				->values(array(
+					'name' => $q->createParameter('server_name'),
+					'publish_url' => $q->createParameter('publishUrl')))
+				->setParameter('server_name', 'unspecified')
+				->setParameter('publishUrl', $arr['configvalue'])
+				->execute();
+
+			// delete old URL config value
+			$q = $this->db->getQueryBuilder();
+			$q->delete('appconfig')
+				->where('appid =' . $q->createParameter('appid'))
+				->andWhere('configkey =' . $q->createParameter('configkey'))
+				->setParameter('appid', 'b2sharebridge')
+				->setParameter('configkey', 'publish_baseurl')
+				->execute();
+
+			// get id of newly added server
+			$q = $this->db->getQueryBuilder();
+			$id = $q->select('id')
+				->from('b2sharebridge_server')
+				->execute()
+				->fetch()['id'];
+
+			// append API key configkeys with server id
+			$q = $this->db->getQueryBuilder();
+			$q->update('preferences')
+					->set('configkey', $q->createParameter('newConfigkey'))
+					->where('configkey = '. $q->createParameter('configkey'))
+					->andWhere('appid = '.$q->createParameter('appid'))
+					->setParameter('configkey', 'token')
+					->setParameter('appid', 'b2sharebridge')
+					->setParameter('newConfigkey', 'token_'.$id)
+					->execute();
+
+			// add server ID to communities
+			$q = $this->db->getQueryBuilder();
+			$q->update('b2sharebridge_communities')
+					->set('server_id', $q->createParameter('server_id'))
+					->setParameter('server_id', $id)
+					->execute();
+		}
 	}
 }

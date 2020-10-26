noblespirit/b2sharebridge-multiserver

Enable using multiple B2SHARE servers
* Admin can configure multiple B2SHARE servers that users can
choose from on the B2SHAREBridge admin settings page
* Servers are saved in their own database table
* API keys are still saved in the user preferences: the key
is appended with an underscore and the server ID

lib/Migration/Version010100Date20200324092922.php
@@ -1,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\B2shareBridge\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\SimpleMigrationStep;
+use OCP\Migration\IOutput;
+
+/**
+ * Auto-generated migration step: Please modify to your needs!
+ */
+class Version010200Date20200324092922 extends SimpleMigrationStep {
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function preSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 * @return null|ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options) {
+		$schema = $schemaClosure();
+
+		$cTable = $schema->getTable('b2sharebridge_communities');
+		$cTable->addColumn('server_id', 'integer', [
+			'notnull' => true,
+		]);
+		$sTable = $schema->getTable('b2sharebridge_status');
+		$sTable->addColumn('server_id', 'integer', [
+			'notnull' => true,
+		]);
+		return $schema;
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+		// TODO: migrate possible existing single-server configuration
+	}
+}

EUDAT-B2DROP/b2sharebridge

lib/Migration/Version010100Date20200324092922.php
@@ -1,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\B2shareBridge\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\SimpleMigrationStep;
+use OCP\Migration\IOutput;
+
+/**
+ * Auto-generated migration step: Please modify to your needs!
+ */
+class Version010100Date20200324092922 extends SimpleMigrationStep {
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function preSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 * @return null|ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options) {
+		$schema = $schemaClosure();
+
+		$cTable = $schema->getTable('b2sharebridge_communities');
+		$cTable->addColumn('server_id', 'integer', [
+			'notnull' => true,
+		]);
+		$sTable = $schema->getTable('b2sharebridge_status');
+		$sTable->addColumn('server_id', 'integer', [
+			'notnull' => true,
+		]);
+		return $schema;
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+		// TODO: migrate possible existing single-server configuration
+	}
+}
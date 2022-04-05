8299f3944b95ab0b02bcb7ffd0a482b3f0fc1140
Sander Apweiler <sa.apweiler@fz-juelich.de>
2022-04-05 12:13:41 +0200

    Disable webpack minimization

--- js/b2sharebridgetabview.js
+++ js/b2sharebridgetabview.js
@@ -1,1 +1,352 @@
-(()=>{"use strict";var e={n:t=>{var i=t&&t.__esModule?()=>t.default:()=>t;return e.d(i,{a:i}),i},d:(t,i)=>{for(var r in i)e.o(i,r)&&!e.o(t,r)&&Object.defineProperty(t,r,{enumerable:!0,get:i[r]})},o:(e,t)=>Object.prototype.hasOwnProperty.call(e,t)};const i=jQuery;var r=e.n(i);!function(){function e(e){if(r()(publish_button).prop("disabled",!0),selectedFiles=FileList.getSelectedFiles(),selectedFiles.length>0)for(index in ids=[],selectedFiles)ids.push(selectedFiles[index].id);else fileInfo=e.data.param,ids=[fileInfo.id];var i=r()("#ddCommunitySelector").val(),n=r()('input[name="open_access"]:checked').length>0,s=r()("#b2s_title").val();r().post(OC.generateUrl("/apps/b2sharebridge/publish"),{ids,community:i,open_access:n,title:s,server_id:r()("#ddServerSelector").val()},(function(e){e&&"success"===e.status&&OC.dialogs.info(t("b2sharebridge",e.message),t("b2sharebridge","Info"))}))}var i=OCA.Files.DetailTabView.extend({id:"b2shareBridgeTabView",className:"b2shareBridgeTabView tab",_label:"b2sharebridge",_loading:!1,_publish_buton_disabled:!1,initialize:function(){OCA.Files.DetailTabView.prototype.initialize.apply(this,arguments),this.collection=new OCA.B2shareBridge.B2shareBridgeCollection,this.collection.setObjectType("files"),this.collection.on("request",this._onRequest,this),this.collection.on("sync",this._onEndRequest,this),this.collection.on("update",this._onChange,this),this.collection.on("error",this._onError,this),this._error_msg="initializing",this._b2s_title="Deposit title here",this.communities=[]},events:{},getLabel:function(){return t("b2sharebridge","B2SHARE")},getIcon:function(){return"icon-filelist"},nextPage:function(){},_onClickShowMoreVersions:function(e){},_onClickRevertVersion:function(e){},_toggleLoading:function(e){},_onRequest:function(){},_onEndRequest:function(){},_onAddModel:function(e){},getTokens:function(){var e=this;if(!this.tokens){var t="/apps/b2sharebridge/apitoken?requesttoken="+encodeURIComponent(oc_requesttoken);r().ajax({type:"GET",url:OC.generateUrl(t),async:!1}).done((function(t){e.tokens=t})).fail((function(e){r()(b2sharebridge_errormsg).html("Fetching tokens failed!"),r()(b2sharebridge_errormsg).show()}))}return this.tokens},getCommunities:function(){var e=this;if(!this.communities.length){var t="/apps/b2sharebridge/gettabviewcontent?requesttoken="+encodeURIComponent(oc_requesttoken);r().ajax({type:"GET",url:OC.generateUrl(t),async:!1}).done((function(t){e.communities=t})).fail((function(e){r()(b2sharebridge_errormsg).html("Fetching B2SHARE communities failed!"),r()(b2sharebridge_errormsg).show()}))}return this.communities},getCommunitySelectorHTML:function(){var e="";return e="<select id='ddCommunitySelector'>",r().each(this.getCommunities().filter((function(e){return e.serverId.toString()===r()("#ddServerSelector").val().toString()})),(function(t,i){e=e+'<option value="'+i.id+'">'+i.name+"</option>"})),e+="</select>"},getServerSelectorHTML:function(){var e="/apps/b2sharebridge/servers?requesttoken="+encodeURIComponent(oc_requesttoken),t="";return r().ajax({type:"GET",url:OC.generateUrl(e),async:!1}).done((function(e){t="<select id='ddServerSelector'>",r().each(e,(function(e,i){t=t+'<option value="'+i.id+'">'+i.name+"</option>"})),t+="</select>"})).fail((function(e){r()(b2sharebridge_errormsg).html("Fetching B2SHARE servers failed!"),r()(b2sharebridge_errormsg).show()})),t},template:function(e){return'<div><div id="b2sharebridgeTabView" class="dialogContainer"><table><tr><td>Title:</td><td><input type="text" name="b2s_title" id="b2s_title"></input></td></tr><tr><td>Server:</td><td><div id="serverSelector"></div></td></tr><tr><td>Community:</td><td><div id="communitySelector"></div></td></tr><tr><td>Open access:</td><td><input type="checkbox" name="open_access" id="cbopen_access" /></td></tr><tr><td></td><td><input type="button" value="deposit" id="publish_button"/></td></tr></table><div class="errormsg" id="b2sharebridge_errormsg">ERROR3</div></div>'},itemTemplate:function(e){},setFileInfo:function(e){e&&(this.fileInfo=e,this.initializeB2ShareUI(e),this.render())},_formatItem:function(e){},checkToken:function(){this.tokens[r()("#ddServerSelector").val()]?r()(b2sharebridge_errormsg).hide():(r()(b2sharebridge_errormsg).html("Please set B2SHARE API token in B2SHARE settings"),r()(b2sharebridge_errormsg).show())},onChangeServer:function(){r()(communitySelector).html(this.getCommunitySelectorHTML()),this.checkToken()},render:function(){this.$el.html(this.template()),r()("#serverSelector").html(this.getServerSelectorHTML()),r()("#communitySelector").html(this.getCommunitySelectorHTML()),this.getTokens(),r()(serverSelector).change(this.onChangeServer.bind(this)),r()(publish_button).bind("click",{param:this.fileInfo},e),r()(publish_button).prop("disabled",this._publish_button_disabled),r()("#b2s_title").val(this._b2s_title),this.delegateEvents(),r()(b2sharebridge_errormsg).html(this._error_msg),""!=this._error_msg?r()(b2sharebridge_errormsg).show():this.checkToken()},canDisplay:function(e){return!!e&&!e.isDirectory()},processData:function(e){this._publish_button_disabled=e.error,this._error_msg=e.error_msg,this._b2s_title=e.title},initializeB2ShareUI:function(e){var t="/apps/b2sharebridge/initializeb2shareui?requesttoken="+encodeURIComponent(oc_requesttoken)+"&file_id="+encodeURIComponent(e.id),i=this;r().ajax({type:"GET",url:OC.generateUrl(t),async:!1}).done((function(e){i.processData(e)})).fail((function(e){i._publish_button_disabled=!0,i._error_msg="ERROR - Nextcloud server cannot be reached."}))}});OCA.B2shareBridge=OCA.B2shareBridge||{},OCA.B2shareBridge.B2shareBridgeTabView=i}()})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/templates/template
+var TEMPLATE =
+    '<div>' +
+        '<div id="b2sharebridgeTabView" class="dialogContainer">' +
+        '<table><tr><td>Title:</td><td><input type="text" name="b2s_title" id="b2s_title"></input></td></tr>' +
+        '<tr><td>Server:</td><td><div id="serverSelector"></div></td></tr>' +
+        '<tr><td>Community:</td><td><div id="communitySelector"></div></td></tr>' +
+        '<tr><td>Open access:</td><td><input type="checkbox" name="open_access" id="cbopen_access" /></td></tr>' +
+        '<tr><td></td><td><input type="button" value="deposit" id="publish_button"/></td></tr></table>' +
+        '<div class="errormsg" id="b2sharebridge_errormsg">ERROR3</div>' +
+    '</div>';
+
+
+;// CONCATENATED MODULE: external "jQuery"
+const external_jQuery_namespaceObject = jQuery;
+var external_jQuery_default = /*#__PURE__*/__webpack_require__.n(external_jQuery_namespaceObject);
+;// CONCATENATED MODULE: ./src/b2sharebridgetabview.js
+
+
+
+(function() {
+
+//    var TEMPLATE =
+//        '<div>' +
+//		'<div id="b2sharebridgeTabView" class="dialogContainer">' +
+//		'<table><tr><td>Title:</td><td><input type="text" name="b2s_title" id="b2s_title"></input></td></tr>' +
+//		'<tr><td>Server:</td><td><div id="serverSelector"></div></td></tr>' +
+//		'<tr><td>Community:</td><td><div id="communitySelector"></div></td></tr>' +
+//		'<tr><td>Open access:</td><td><input type="checkbox" name="open_access" id="cbopen_access" /></td></tr>' +
+//		'<tr><td></td><td><input type="button" value="deposit" id="publish_button"/></td></tr></table>' +
+//		'<div class="errormsg" id="b2sharebridge_errormsg">ERROR3</div>' +
+//        '</div>';
+
+        function publishAction(e){
+			external_jQuery_default()(publish_button).prop('disabled', true);
+			selectedFiles = FileList.getSelectedFiles();
+			// if selectedFiles is empty, use fileInfo
+			// otherwise create an array of files from the selection
+			if (selectedFiles.length>0){
+				ids = []
+				for (index in selectedFiles){
+					ids.push(selectedFiles[index].id)
+				}
+			} else {
+            	fileInfo = e.data.param;
+				ids = [fileInfo.id];
+			}
+            var selected_community = external_jQuery_default()("#ddCommunitySelector").val();
+			var open_access = external_jQuery_default()('input[name="open_access"]:checked').length > 0;
+			var title = external_jQuery_default()("#b2s_title").val();
+            external_jQuery_default().post(
+                OC.generateUrl('/apps/b2sharebridge/publish'),
+                {
+                    ids: ids,
+                    community: selected_community,
+					open_access: open_access,
+                    title: title,
+                    server_id: external_jQuery_default()('#ddServerSelector').val()
+                },
+                function (result) {
+                    if (result && result.status === 'success') {
+                        OC.dialogs.info(
+                            t('b2sharebridge', result.message),
+                            t('b2sharebridge', 'Info'));
+                    }
+                });
+        }
+
+
+    /**
+     * @class OCA.B2shareBridge.B2shareBridgeTabView
+     * @memberof OCA.B2shareBridge
+     * @classdesc
+     *
+     * Shows publication (to b2share) information for file
+     *
+     */
+    var B2shareBridgeTabView = OCA.Files.DetailTabView.extend(
+        /** @lends OCA.B2shareBridge.B2shareBridgeTabView.prototype */{
+        id: 'b2shareBridgeTabView',
+        className: 'b2shareBridgeTabView tab',
+
+        _label: 'b2sharebridge',
+
+        _loading: false,
+
+		_publish_buton_disabled: false,
+
+
+        initialize: function() {
+            OCA.Files.DetailTabView.prototype.initialize.apply(this, arguments);
+            this.collection = new OCA.B2shareBridge.B2shareBridgeCollection();
+            this.collection.setObjectType('files');
+            this.collection.on('request', this._onRequest, this);
+            this.collection.on('sync', this._onEndRequest, this);
+            this.collection.on('update', this._onChange, this);
+            this.collection.on('error', this._onError, this);
+			this._error_msg = "initializing";
+            this._b2s_title = "Deposit title here";
+            this.communities = [];
+        },
+
+        events: {
+        },
+
+
+        getLabel: function() {
+            return t('b2sharebridge', 'B2SHARE');
+        },
+
+        getIcon: function() {
+           return 'icon-filelist';
+        },
+
+        nextPage: function() {
+        },
+
+        _onClickShowMoreVersions: function(ev) {
+        },
+
+        _onClickRevertVersion: function(ev) {
+        },
+
+        _toggleLoading: function(state) {
+        },
+
+        _onRequest: function() {
+        },
+
+        _onEndRequest: function() {
+        },
+
+        _onAddModel: function(model) {
+        },
+
+        getTokens: function() {
+            var that = this;
+            if (!this.tokens) {
+                var url_path =
+                "/apps/b2sharebridge/apitoken?requesttoken=" +
+                encodeURIComponent(oc_requesttoken);
+                external_jQuery_default().ajax({
+                    type: 'GET',
+                    url: OC.generateUrl(url_path),
+                    async: false
+                }).done(function(data) {
+                    that.tokens = data;
+                }).fail(function(data){
+                    external_jQuery_default()(b2sharebridge_errormsg).html('Fetching tokens failed!');
+                    external_jQuery_default()(b2sharebridge_errormsg).show();
+                });
+            }
+            return this.tokens;
+        },
+
+        getCommunities: function() {
+            var that = this;
+            if (!this.communities.length) {
+                var url_path =
+                "/apps/b2sharebridge/gettabviewcontent?requesttoken=" +
+                encodeURIComponent(oc_requesttoken);
+                external_jQuery_default().ajax({
+                    type: 'GET',
+                    url: OC.generateUrl(url_path),
+                    async: false
+                }).done(function(data) {
+                    that.communities = data;
+                }).fail(function(data){
+                    external_jQuery_default()(b2sharebridge_errormsg).html('Fetching B2SHARE communities failed!');
+                    external_jQuery_default()(b2sharebridge_errormsg).show();
+                });
+            }
+            return this.communities;
+        },
+
+        getCommunitySelectorHTML: function() {
+            var result = "";
+            result = "<select id='ddCommunitySelector'>";
+            external_jQuery_default().each(
+                this.getCommunities().filter(function(community) {
+                    return community.serverId.toString() === external_jQuery_default()('#ddServerSelector').val().toString();
+                }),
+                function(i, c) {
+                    result = result + "<option value=\"" + c.id + "\">"+ c.name + "</option>";
+                }
+            );
+            result = result + "</select>"
+            return result;
+        },
+
+        getServerSelectorHTML: function() {
+            var url_path =
+                "/apps/b2sharebridge/servers?requesttoken=" +
+                encodeURIComponent(oc_requesttoken);
+            var result = "";
+            external_jQuery_default().ajax({
+                type: 'GET',
+                url: OC.generateUrl(url_path),
+                async: false
+            }).done(function(data){
+                result = "<select id='ddServerSelector'>";
+                external_jQuery_default().each(data, function(key, value){
+                    result = result + "<option value=\"" + value.id + "\">"+ value.name + "</option>";
+                });
+                result = result + "</select>"
+            }).fail(function(data){
+                external_jQuery_default()(b2sharebridge_errormsg).html('Fetching B2SHARE servers failed!');
+                external_jQuery_default()(b2sharebridge_errormsg).show();
+            });
+            return result;
+        },
+
+
+        template: function(data) {
+            return TEMPLATE;
+        },
+
+        itemTemplate: function(data) {
+        },
+
+        setFileInfo: function(fileInfo) {
+            if (fileInfo) {
+                this.fileInfo = fileInfo;
+                this.initializeB2ShareUI(fileInfo);
+                this.render();
+            }
+        },
+
+        _formatItem: function(version) {
+        },
+
+        checkToken: function() {
+            if (!this.tokens[external_jQuery_default()('#ddServerSelector').val()]) {
+                external_jQuery_default()(b2sharebridge_errormsg).html('Please set B2SHARE API token in B2SHARE settings');
+                external_jQuery_default()(b2sharebridge_errormsg).show();
+            } else {
+                external_jQuery_default()(b2sharebridge_errormsg).hide();
+            }
+        },
+
+        onChangeServer: function() {
+            external_jQuery_default()(communitySelector).html(this.getCommunitySelectorHTML());
+            this.checkToken();
+        },
+
+        /**
+         * Renders this details view
+         */
+        render: function() {
+            this.$el.html(this.template());
+            external_jQuery_default()("#serverSelector").html(this.getServerSelectorHTML());
+            external_jQuery_default()("#communitySelector").html(this.getCommunitySelectorHTML());
+            this.getTokens();
+            external_jQuery_default()(serverSelector).change(this.onChangeServer.bind(this));
+            external_jQuery_default()(publish_button).bind('click',{param: this.fileInfo}, publishAction);
+			external_jQuery_default()(publish_button).prop('disabled', this._publish_button_disabled);
+			external_jQuery_default()("#b2s_title").val(this._b2s_title);
+            this.delegateEvents();
+			external_jQuery_default()(b2sharebridge_errormsg).html(this._error_msg);
+			if (this._error_msg!=""){
+				external_jQuery_default()(b2sharebridge_errormsg).show();
+			} else {
+                this.checkToken();
+            }
+        },
+
+        /**
+         * Returns true for files, false for folders.
+         *
+         * @return {bool} true for files, false for folders
+         */
+        canDisplay: function(fileInfo) {
+            if (!fileInfo) {
+                return false;
+            }
+            return !fileInfo.isDirectory();
+        },
+
+        processData: function(data) {
+			this._publish_button_disabled = data['error'];
+			this._error_msg = data['error_msg'];
+			this._b2s_title = data['title'];
+		},
+
+        initializeB2ShareUI: function(fileInfo){
+            var url_path =
+                "/apps/b2sharebridge/initializeb2shareui?requesttoken=" +
+                encodeURIComponent(oc_requesttoken) + "&file_id=" +
+                encodeURIComponent(fileInfo.id);
+            var communities = [];
+            var result = "";
+            var that = this;
+            external_jQuery_default().ajax({
+                type: 'GET',
+                url: OC.generateUrl(url_path),
+                async: false
+            }).done(function(data){
+                that.processData(data);
+            }).fail(function(data){
+                //if PHP not reachable, disable publish button
+                that._publish_button_disabled = true;
+                that._error_msg = "ERROR - Nextcloud server cannot be reached."
+            });
+    		}
+    });
+
+    OCA.B2shareBridge = OCA.B2shareBridge || {};
+
+    OCA.B2shareBridge.B2shareBridgeTabView = B2shareBridgeTabView;
+})();
+
+
+/******/ })()
+;
--- webpack.config.js
+++ webpack.config.js
@@ -12,4 +12,7 @@ module.exports = {
     externals: {
             jquery: 'jQuery'
         },
+    optimization: {
+            minimize: false
+        },
 }

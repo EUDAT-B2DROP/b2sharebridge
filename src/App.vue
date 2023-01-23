0418deac7abc075af02b406280ca0acdae042fd7
Marvin Winkens <m.winkens@fz-juelich.de>
2023-01-23 10:16:53 +0100

    properly load deposits in order to show them

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,41 @@
 <template>
-  <NcContent appName="b2sharebridge">
-    <div id="content" class="app-b2sharebridge">
-      <NcAppNavigation>
-        <NcAppNavigationNew v-if="!loading"
-                            :text="t('b2sharebridge', 'All DepositsTODORemoveTest')"
-                            :disabled="false"
-                            button-id="deposit-all-button"
-                            button-class="icon-add"
-                            @click="showAllDeposits"/>
-        <NcAppNavigationNew v-if="!loading"
-                            :text="t('b2sharebridge', 'Pending Deposits')"
-                            :disabled="false"
-                            button-id="deposit-pending-button"
-                            button-class="icon-add"
-                            @click="showPendingDeposits"/>
-        <NcAppNavigationNew v-if="!loading"
-                            :text="t('b2sharebridge', 'Published Deposits')"
-                            :disabled="false"
-                            button-id="deposit-published-button"
-                            button-class="icon-add"
-                            @click="showPublishedDeposits"/>
-        <NcAppNavigationNew v-if="!loading"
-                            :text="t('b2sharebridge', 'Failed Deposits')"
-                            :disabled="false"
-                            button-id="deposit-failed-button"
-                            button-class="icon-add"
-                            @click="showFailedDeposits"/>
-      </NcAppNavigation>
-      <NcAppContent>
-        <div v-if="Deposits.length === 0">
-          <div class="icon-file"/>
-          <h2 style="text-align: center;">{{ t('b2sharebridge', 'Create a deposit to get started') }}</h2>
-        </div>
-        <div v-else>
-          <b-table striped hover :deposits="Deposits"></b-table>
-        </div>
-      </NcAppContent>
-    </div>
-  </NcContent>
+  <div id="content" class="app-b2sharebridge">
+    <NcAppNavigation>
+      <NcAppNavigationNew v-if="!loading"
+                          :text="t('b2sharebridge', 'All DepositsTODORemoveTest')"
+                          :disabled="false"
+                          button-id="deposit-all-button"
+                          button-class="icon-add"
+                          @click="showAllDeposits"/>
+      <NcAppNavigationNew v-if="!loading"
+                          :text="t('b2sharebridge', 'Pending Deposits')"
+                          :disabled="false"
+                          button-id="deposit-pending-button"
+                          button-class="icon-add"
+                          @click="showPendingDeposits"/>
+      <NcAppNavigationNew v-if="!loading"
+                          :text="t('b2sharebridge', 'Published Deposits')"
+                          :disabled="false"
+                          button-id="deposit-published-button"
+                          button-class="icon-add"
+                          @click="showPublishedDeposits"/>
+      <NcAppNavigationNew v-if="!loading"
+                          :text="t('b2sharebridge', 'Failed Deposits')"
+                          :disabled="false"
+                          button-id="deposit-failed-button"
+                          button-class="icon-add"
+                          @click="showFailedDeposits"/>
+    </NcAppNavigation>
+    <NcAppContent>
+      <div v-if="Deposits.length === 0">
+        <div class="icon-file"/>
+        <h2 style="text-align: center;">{{ t('b2sharebridge', 'Create a deposit to get started') }}</h2>
+      </div>
+      <div v-else>
+        <b-table striped hover :deposits="Deposits"></b-table>
+      </div>
+    </NcAppContent>
+  </div>
 </template>
 <script>
 import '@nextcloud/dialogs/styles/toast.scss'
@@ -46,7 +44,6 @@ import axios from '@nextcloud/axios'
 import {generateUrl} from '@nextcloud/router'
 import {showError, showSuccess} from '@nextcloud/dialogs'
 import {
-  NcContent,
   NcActionButton,
   NcAppContent,
   NcAppNavigation,
@@ -75,13 +72,40 @@ export default {
    */
   async mounted() {
     try {
-      const response = await axios.get(generateUrl('/apps/b2sharebridge/')); //TODO
-      this.Deposits = response.data
+      await this.loadDeposits("all")
     } catch (e) {
       console.error(e)
       showError(t('b2sharebridge', 'Could not fetch deposits'))
     }
     this.loading = false
+  },
+
+  loadDeposits(filter) {
+    return axios
+        .get('/apps/b2sharebridge/depositList', { params: filter})
+        .then((response) => {
+          this.Deposits = response.data
+        })
+        .catch((error) => {
+          console.error(error)
+          console.error("Could not load deposit List")
+        })
+  },
+
+  showAllDeposits() {
+    this.loadDeposits("all")
+  },
+
+  showPendingDeposits() {
+    this.loadDeposits("pending")
+  },
+
+  showPublishedDeposits() {
+    this.loadDeposits("published")
+  },
+
+  showFailedDeposits() {
+    this.loadDeposits("failed")
   },
 }
 </script>

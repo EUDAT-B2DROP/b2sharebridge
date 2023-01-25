3bc51aa5211fc264d200652e7a28fcc1f053ba3b
Marvin Winkens <m.winkens@fz-juelich.de>
2023-01-25 10:26:44 +0100

    switch to json

--- lib/Controller/ViewController.php
+++ lib/Controller/ViewController.php
@@ -125,18 +125,18 @@ class ViewController extends Controller
      *     'publish': get published deposits
      *     'failed': get failed deposits
      *
-     * @return DataResponse
+     * @return JSONResponse
      *
      * @throws Exception
      * @throws DoesNotExistException
      * @throws MultipleObjectsReturnedException
      * @NoAdminRequired
      */
-    public function depositList(): DataResponse
+    public function depositList(): JSONResponse
     {
         $param = $this->request->getParams();
         if (!array_key_exists('filter', $param)) {
-            return new DataResponse(
+            return new JSONResponse(
                 [
                     "message" => "missing argument: filter",
                     "status" => "error"
@@ -156,11 +156,9 @@ class ViewController extends Controller
             $publication->setFileCount(
                 $this->fdmapper->getFileCount($publication->getId())
             );
-            $publication = (array)$publication;  //Note this should be json encoded if a deposit would be deeply nested
         }
 
-
-        return new DataResponse($publications);
+        return new JSONResponse(json_encode($publications));
     }
 
     /**
